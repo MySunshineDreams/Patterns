d86a9201f4455c6156783afca405b2bf31c72a04
SunshineDreams <mysunshinedreams@163.com>
2017-01-10 17:40:35 +0800

    1. 懒汉单例模式 2. 恶汉单例模式

---
+++ patterns-build/src/test/java/com/sunshine/patterns/singleton/SingletonTest.java
@@ -0,0 +1,46 @@
+package com.sunshine.patterns.singleton;
+
+import javax.swing.text.DefaultEditorKit;
+import java.lang.reflect.Field;
+
+/**
+ * Created by <sunshine> mysunshinedreams@163.com on 2017/1/10 0010.
+ */
+public class SingletonTest {
+
+	public static void main(String[] args) {
+		//lazySingletonTest();
+		hungrySingletonTest();
+	}
+
+	private static void lazySingletonTest() {
+		try {
+			LazySingleton lazySingleton = LazySingleton.getInstantce();
+			System.out.println(LazySingleton.getLazySingleton());
+			Field[] fields = lazySingleton.getClass().getDeclaredFields();
+			for (Field field : fields) {
+				field.setAccessible(true);
+				System.out.println(field.getName());
+				field.set("lazySingleton", null);
+			}
+			System.out.println(LazySingleton.getLazySingleton());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void hungrySingletonTest() {
+		try {
+			HungrySingleton hungrySingleton = HungrySingleton.getInstance();
+			System.out.println(HungrySingleton.getInstance());
+			Field[] fields = HungrySingleton.class.getFields();
+			for (Field field : fields) {
+				field.setAccessible(true);
+				System.out.println(field.getName());
+			}
+			System.out.println(HungrySingleton.getInstance());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
