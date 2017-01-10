deb91f9fb5b4f2f0a158569e7b90d0827a1d4ed2
SunshineDreams <mysunshinedreams@163.com>
2017-01-10 11:45:25 +0800

    策略模式

---
+++ patterns-build/src/main/java/com/sunshine/patterns/strategy/Strategy/Push.java
@@ -0,0 +1,11 @@
+package com.sunshine.patterns.strategy.Strategy;
+
+/**
+ * Created by <sunshine> mysunshinedreams@163.com on 2017/1/10 0010.
+ */
+public interface Push {
+	/**
+	 *
+	 */
+	public void push();
+}
