ae890964f9128114cba0d1ddf04220af947475b3
SunshineDreams <mysunshinedreams@163.com>
2017-02-07 11:29:14 +0800

    工厂模式优化

--- patterns-build/src/main/java/com/sunshine/patterns/factory/factory/ElderGroupFactory.java
+++ patterns-build/src/main/java/com/sunshine/patterns/factory/factory/ElderGroupFactory.java
@@ -2,6 +2,7 @@ package com.sunshine.patterns.factory.factory;
 
 /**
  * Created by <sunshine> mysunshinedreams@163.com on 2017/1/12 0012.
+ * 工厂模式——具体工厂
  */
 public class ElderGroupFactory extends GroupFactory {
 	@Override
--- patterns-build/src/main/java/com/sunshine/patterns/factory/simple/TeenageGroup.java
+++ patterns-build/src/main/java/com/sunshine/patterns/factory/simple/TeenageGroup.java
@@ -2,6 +2,7 @@ package com.sunshine.patterns.factory.simple;
 
 /**
  * Created by <sunshine> mysunshinedreams@163.com on 2017/1/12 0012.
+ * 简单工厂模式——具体产品
  */
 public class TeenageGroup extends Group {
 
