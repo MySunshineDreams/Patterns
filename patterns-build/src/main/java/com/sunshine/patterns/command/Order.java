d4cb5f4f34c11a5c1057a55d1d9d82af07a5a432
Sunshine <wb-yangsonglin@le.com>
2017-03-28 17:07:02 +0800

    命令模式订正

--- patterns-build/src/main/java/com/sunshine/patterns/command/Order.java
+++ patterns-build/src/main/java/com/sunshine/patterns/command/Order.java
@@ -2,7 +2,7 @@ package com.sunshine.patterns.command;
 
 /**
  * Created by <sunshine> mysunshinedreams@163.com on 2017/1/20 0020.
- * 命令模式——抽象命令类
+ * 命令模式——抽象命令
  */
 public abstract class Order {
 	public abstract void execute();
