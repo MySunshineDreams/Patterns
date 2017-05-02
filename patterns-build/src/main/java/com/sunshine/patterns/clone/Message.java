959a6ad08eeaf022202901baf6e80fc1331de673
Sunshine <wb-yangsonglin@le.com>
2017-05-02 23:58:36 +0800

    克隆模式订正

--- patterns-build/src/main/java/com/sunshine/patterns/clone/Message.java
+++ patterns-build/src/main/java/com/sunshine/patterns/clone/Message.java
@@ -2,6 +2,7 @@ package com.sunshine.patterns.clone;
 
 /**
  * Created by <sunshine> mysunshinedreams@163.com on 2017/1/13 0013.
+ * 原型模式——具体原型类
  */
 public class Message implements Cloneable {
 	private String title;
